45e98fdc39d898219eb3e5c7e7783de1c5961af7
sasanktumpati <sasanktumpati16@gmail.com>
2025-06-11 02:06:50 +0530

    [fix] changes server port

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=35000,
+        port=8000,
         reload=settings.debug or args.reload,
         log_level="debug" if settings.debug else "info",
     )
